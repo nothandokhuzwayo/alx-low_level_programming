e7738da50d80e353c17a3b56804ccfc5d4b78cd8
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-22 13:19:15 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to the standard error
+ *
+ * Return: 0 (Achieved)
+ */
+int main(void)
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
