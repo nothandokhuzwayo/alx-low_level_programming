c955a2d0f249278744df4595afbeef3226681559
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-22 18:31:27 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase and then in uppercase
+ * using the putchar function
+ * Return: 0 (Achieved)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')
+	{
+			putchar(d);
+			d++;
+	}
+	putchar('\n')
+		return (0);
+}
