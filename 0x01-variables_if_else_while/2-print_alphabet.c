c2669847a9dd899212d40369d5c2d3de5382ccec
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-22 18:12:26 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints an alphabet in lowercase using putchar function
+ * Return: 0 (Achieved)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n')
+		return (0);
+}
+
