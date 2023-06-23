281a0419dbc8255659bff8329ea6dd0d5d4ed1d6
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 15:36:10 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,9 @@
 /**
  * _islower - program that checks if a character is lowercase
  * @c: the character to check
- * Return 1 if character is lowercase, return 0 if not
+ * Return: 1 if character is lowercase, return 0 if not
  */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
