12098b320a25c049b113ef7a995f9908b5fb4215
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 16:12:11 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		return ('1');
 	}
 	else if (n == 0)
 	{
@@ -22,6 +22,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (-1);
+		return ('-1');
 	}
 }
