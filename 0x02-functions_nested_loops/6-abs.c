f70411acf6e529d39aedcecf04f63a005e1c303a
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 16:23:04 -0700

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - program that prints last digit
+ * @n: the number in question
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int ld = n & 10;
+
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld * '0');
+
+	return (0);
+}
