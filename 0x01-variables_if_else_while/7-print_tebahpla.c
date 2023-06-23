d18709fbf794eb7b04b7d839b74417cef5008c95
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-22 19:19:04 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the lowercase alphabet in reverse using putchar
+ * Return: 0 (Achieved)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
