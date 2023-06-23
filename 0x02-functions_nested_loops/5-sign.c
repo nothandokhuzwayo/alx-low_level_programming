76772fc86bc6d1161014ad549dcc2f69a41ac24c
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 16:02:16 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - program that prints the sign of a number
+ * @n: number to print
+ * Return: 1 if the number is greater than zero, 0 if number is equal to zero
+ * -1 if the number is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return ('-0');
+	}
+	else(n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
