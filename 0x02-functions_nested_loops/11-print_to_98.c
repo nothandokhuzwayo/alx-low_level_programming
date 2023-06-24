b36194eedfee0432aa8a2cd862a682a6514ca635
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 17:53:51 -0700

    7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf
