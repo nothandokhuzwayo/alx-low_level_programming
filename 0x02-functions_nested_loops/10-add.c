872a048cfefe9f88f7d3d57fbf25ffe673e38685
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 17:40:32 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - program that adds two integers and returns the results
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
