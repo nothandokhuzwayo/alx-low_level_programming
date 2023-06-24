8c3222cf0bdbd563eb4286f59bf7a8c212a30d5f
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 21:01:20 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers from 0 to 9 using putchar
+ * Return: 0 (Achieved)
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + '0');
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
