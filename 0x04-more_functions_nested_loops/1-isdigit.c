e4291df72347698cc700969e1be4187d2a661b5f
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-23 20:01:10 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit
+ * @c: char to check
+ * Return: 1 (Achieved) else return 0
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
