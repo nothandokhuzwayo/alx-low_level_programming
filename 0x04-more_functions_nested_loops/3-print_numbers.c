f0ba0bba444814482974d722c26875706a791351
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-25 12:45:40 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - function that prints the numbers from 0 to 9 using putchar
- * Return: void
+ * Return: 0 (Achieved)
  */
 
 void print_numbers(void)
 {
-	char c;
+	int num = 0;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
