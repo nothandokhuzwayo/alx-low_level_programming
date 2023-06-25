a79df30f51fa8d1b9a261c0887d03dc66c05ac42
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-25 15:20:05 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
-#print "main.h"
+#include "main.h"
 
 /**
  * print_most_numbers - Function that prints the numbers 0 to 9 except 2 and 4
- * return: void
+ * Return: void
  */
 
 void print_most_numbers(void)
