9da77654378de3e18e3eb8b4fe3c3e66dc20fef1
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-25 10:21:42 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,15 @@
 /**
  * _isupper - function that checks for uppercase character
  * @c: the char to be checked
- * Return: 1 if uppercase, otherwise return 0
+ * Return: 1 if uppercase otherwise return 0
  */
 
 int _isupper(int c)
 {
 	if (c >= '65' && c <= '90')
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
