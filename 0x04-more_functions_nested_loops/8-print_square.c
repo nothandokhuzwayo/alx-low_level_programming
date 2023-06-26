b705c9b516280100b49cc0ffaa5f66c011ac175b
tha-ndo <nothando.khuzwayo@icloud.com>
2023-06-25 18:09:57 -0700

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,13 @@ void print_square(int size)
 	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 0; i < (size); j++)
+	for (i = 0; j < (size); i++)
 	{
-		_putchar('#');
+
+		for (i = 0; i < (size); j++)
+		{
+			_putchar('#');
 	}
 	_putchar('\n');
+}
 }
