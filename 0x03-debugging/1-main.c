014dad0c9b9c15c305f829b18fa9c3c197c03fda
tha-ndo <nothando.khuzwayo@icloud.com>
2023-07-03 19:49:00 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
@@ -17,6 +17,8 @@ int main(void)
 	{
 		putchar(i);
 	}
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
