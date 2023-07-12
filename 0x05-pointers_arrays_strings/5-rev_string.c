becfa01452ca6e56f7199e9dfbcef2a86b46b918
tha-ndo <nothando.khuzwayo@icloud.com>
2023-07-12 08:20:05 -0700

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "man.h"
+
+/**
+ * print_rev - function that reverses a string
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
