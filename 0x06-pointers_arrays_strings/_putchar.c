8971c8c501f1f30d877f4f3dc65eb21385238db7
tha-ndo <nothando.khuzwayo@icloud.com>
2023-07-13 04:11:24 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: the character to be printed
  * Return: On success 1.
  * Upon error,return -1, and errno is set appropriately.
  */
