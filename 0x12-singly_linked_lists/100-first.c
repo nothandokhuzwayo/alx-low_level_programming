5a840f821d126790f7a51aac7900a4dcd3331bae
tha-ndo <nothando.khuzwayo@icloud.com>
2023-07-28 18:22:53 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+void message_to_be_printed(void) __attribute__((constructor));
+/**
+ * main_message - function that prints a message
+ */
+void main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+			printf("I bore my house upon my back!\n");
+}
