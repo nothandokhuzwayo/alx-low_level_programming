0585c4c47911babc6df96760c96bae91435b54d8
tha-ndo <nothando.khuzwayo@icloud.com>
2023-07-30 12:59:48 -0700

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+/**
+ * file: 3-function_like_macro.h
+ * @x: input
+ * description: that computes the absolute value of a specified number
+ */
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
