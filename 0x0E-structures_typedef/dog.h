21b773c27b212c4a287173d96388d904ce2d7996
tha-ndo <nothando.khuzwayo@icloud.com>
2023-07-31 12:00:23 -0700

    Defining a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct that defines a new types
+ *
+ * @name: the name of the dog
+ * @age:how old the dog is
+ * @owner:the owner of the dog
+ *
+ * description: struct, a new function
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
